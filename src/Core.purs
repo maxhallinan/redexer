f640089f4cfa8fc4ada949cf62db5cacbf658f98
Max Hallinan <maxhallinan@riseup.net>
2019-09-03 16:33:33 -0400

    Implement Core.replaceNode.

--- src/Core.purs
+++ src/Core.purs
@@ -1,17 +1,24 @@
-module Core(ApplyErr(..), Expr(..), Node, applyLambda, eqNode) where
+module Core
+  ( ApplyErr(..)
+  , Expr(..)
+  , Node
+  , ReplaceErr(..)
+  , applyLambda
+  , eqNode
+  , replaceNode
+  ) where
 
 import Prelude
 
+import Data.Maybe (Maybe(..), maybe)
 import Data.Either (Either(..))
-import Data.Tuple (Tuple)
 
 type Node = { id :: String, expr :: Expr }
 
 eqNode :: Node -> Node -> Boolean
 eqNode n1 n2 = eq n1.expr n2.expr
 
-
-data Expr 
+data Expr
   = Var String
   | Lambda String Node
   | Apply Node Node
@@ -27,30 +34,78 @@ instance eqExpr :: Eq Expr where
   eq (Apply e1 e2) (Apply e3 e4) = (eqNode e1 e3) && (eqNode e2 e4)
   eq _ _ = false
 
+data ReplaceErr = NodeNotFound String
+derive instance eqReplaceErr :: Eq ReplaceErr
+
+instance showReplaceErr :: Show ReplaceErr where
+  show (NodeNotFound nodeId) = "NodeNotFound " <> nodeId
+
+replaceNode :: String -> Node -> Node -> Either ReplaceErr Node
+replaceNode id new old =
+  let
+      replace = replaceNode id new
+  in
+  case old.expr of
+    Var _ ->
+      if old.id == id
+      then pure new
+      else Left (NodeNotFound id)
+    Lambda param body ->
+      if old.id == id
+      then pure new
+      else do
+        body' <- replace body
+        pure { id: old.id, expr: Lambda param body' }
+    Apply n1 n2 ->
+      if old.id == id
+      then pure new
+      else do
+        let toApplyNode x y = { id: old.id, expr: Apply x y }
+        case replace n1 of
+          Left _ -> do
+            n2' <- replace n2
+            pure $ toApplyNode n1 n2'
+          Right n1' ->
+            pure $ toApplyNode n1' n2
+
+findNode :: String -> Node -> Maybe Node
+findNode id tree =
+  case tree.expr of
+    Var _ ->
+      if tree.id == id
+      then pure tree
+      else Nothing
+    Lambda param body ->
+      if tree.id == id
+      then pure tree
+      else findNode id body
+    Apply e1 e2 ->
+      if tree.id == id
+      then pure tree
+      else maybe (findNode id e2) pure $ findNode id e1
+
 data ApplyErr = NotALambda
+derive instance eqApplyErr :: Eq ApplyErr
 
 instance showApplyErr :: Show ApplyErr where
   show NotALambda = "Not a lambda."
-
-instance eqApplyErr :: Eq ApplyErr where
-  eq NotALambda NotALambda = true
 
 applyLambda :: Node -> Node -> Either ApplyErr Node
 applyLambda fn arg =
   case fn.expr of
     Lambda paramName body ->
-      Right $ subParam paramName body arg
+      Right $ subParam paramName body
     _ ->
       Left NotALambda
-  where 
-    subParam :: String -> Node -> Node -> Node
-    subParam paramName body arg =
+  where
+    subParam :: String -> Node -> Node
+    subParam paramName body =
       case body.expr of
         Var varName ->
           if paramName == varName
           then arg
           else body
         Lambda p b ->
-          { id: body.id, expr: Lambda p (subParam paramName b arg) }
+          { id: body.id, expr: Lambda p (subParam paramName b) }
         Apply n1 n2 ->
-          { id: body.id, expr: Apply (subParam paramName n1 arg) (subParam paramName n2 arg) }
+          { id: body.id, expr: Apply (subParam paramName n1) (subParam paramName n2) }
