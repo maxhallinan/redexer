974e8a1d6acfd2a3ba0eee76c78b34119e2490ed
Max Hallinan <maxhallinan@riseup.net>
2019-09-05 11:02:49 -0400

    Parse a placeholder lambda string when Stepper component initializes.

--- src/Stepper.purs
+++ src/Stepper.purs
@@ -2,37 +2,55 @@ module Stepper (component) where
 
 import Prelude
 
-import Core (Node)
+import Core as Core
+import Parse as Parse
+import Data.Array (snoc)
+import Data.Either (Either(..))
 import Data.Maybe (Maybe(..))
+import Effect.Aff (Aff)
+import Effect.Class (liftEffect)
 import Halogen as H
-import Halogen.Component as HC
 import Halogen.HTML as HH
-import Halogen.HTML.Events as HE
-import Halogen.HTML.Properties as HP
 
-type State = { lines :: Array Node }
+type State = { lines :: Array Core.Node }
 
-data Action = Act
+data Action = Initialize
 
-component :: forall q i o m. H.Component HH.HTML q i o m
+component :: forall q i o. H.Component HH.HTML q i o Aff
 component = 
   H.mkComponent 
     { initialState: const initialState
     , render
-    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction }
+    , eval: H.mkEval $ H.defaultEval { handleAction = handleAction 
+                                     , initialize = initialize
+                                     }
     }
 
 initialState :: State
 initialState = { lines: mempty }
 
-render :: forall m. State -> H.ComponentHTML Action () m
-render state =
+render :: State -> H.ComponentHTML Action () Aff
+render state = do
   HH.div 
     [] 
     [ HH.textarea []
     ]
 
-handleAction :: forall o m. Action -> H.HalogenM State Action () o m Unit
+handleAction :: forall o. Action -> H.HalogenM State Action () o Aff Unit
 handleAction = case _ of
-  Act ->
-    H.modify_ identity  
+  Initialize ->
+    handleInitialize
+
+initialize :: Maybe Action
+initialize = Just Initialize
+
+handleInitialize :: forall o. H.HalogenM State Action () o Aff Unit
+handleInitialize = do
+    let parsed = Parse.parse "((\\x.\\y.x a) b)"
+    case parsed of
+      Left err ->
+        -- TODO handle parse error
+        pure unit
+      Right ast -> do
+        ast' <- liftEffect $ Core.replaceIds ast
+        H.modify_ (\s -> { lines: snoc s.lines ast' })
